714ebfb4d6719b2e64b78648cd31828d8d0a7a26
bretWorthy <bretjr88@gmail.com>
2020-01-04 21:34:24 -0500

    new branch first commit

--- src/InstagramClone.js
+++ src/InstagramClone.js
@@ -3,11 +3,6 @@ import { View, StyleSheet } from 'react-native';
 import { MainFeed, Login } from './components/screens';
 import { SwitchNavigator, TabNavigator } from 'react-navigation';
 
-const MainStack = SwitchNavigator({
-    login: Login,
-    main: MainFeed
-});
-
 class InstagramClone extends Component {
 
     render() {
